965f311470a0956d05c23f3565816a6a0f75c1fc
Vinod Patidar <pvinod813@gmail.com>
2021-05-27 12:29:33 +0530

    Initial check-in of module PatientPostgres

---
+++ TodoPostgres/DataAccess/IDataAccessProvider.cs
@@ -0,0 +1,13 @@
+﻿using Patients.Models;
+using System.Collections.Generic;
+namespace Patients.DataAccess
+{
+    public interface IDataAccessProvider
+    {
+        void AddPatientRecord(Patient patient);
+        void UpdatePatientRecord(Patient patient);
+        void DeletePatientRecord(string id);
+        Patient GetPatientSingleRecord(string id);
+        List<Patient> GetPatientRecords();
+    }
+}
---
+++ TodoPostgres/Models/Patient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Patients.Models
+{
+    public class Patient
+    {
+       public string id { get; set; }
+       public string name { get; set; }
+       public string address { get; set; }
+       public string city { get; set; }
+       public float age { get; set; }
+       public string gender { get; set; }
+    }
+}
